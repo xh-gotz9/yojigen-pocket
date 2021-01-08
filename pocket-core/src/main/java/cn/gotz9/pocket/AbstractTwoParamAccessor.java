e50ba21ac555762f659e954e337df4b038a7d30e
xh-gotz9 <xh353127981@gmail.com>
2021-01-08 21:19:47 +0800

    add AbstractTwoParamAccessor into core module

---
+++ pocket-core/src/main/java/cn/gotz9/pocket/AbstractTwoParamAccessor.java
@@ -0,0 +1,41 @@
+package cn.gotz9.pocket;
+
+import cn.gotz9.pocket.codec.CodecUtil;
+import cn.gotz9.pocket.codec.ValueCodec;
+import cn.gotz9.pocket.storage.*;
+
+import java.util.Optional;
+
+public abstract class AbstractTwoParamAccessor<T, F, S> implements TwoParamAccessor<T, F, S> {
+
+    private final ValueCodec<T> codec;
+
+    private final TwoParamByteStorage<F, S> byteStorage;
+
+    public AbstractTwoParamAccessor(ValueCodec<T> codec, TwoParamByteStorage<F, S> byteStorage) {
+        this.codec = codec;
+        this.byteStorage = byteStorage;
+    }
+
+    public <I, J> AbstractTwoParamAccessor(ValueCodec<T> codec, TwoParamByteStorage<I, J> byteStorage, ParamConverter<F, I> converter1, ParamConverter<S, J> converter2) {
+        this(codec, new TwoParamByteStorageProxy<>(byteStorage, converter1, converter2));
+    }
+
+    @Override
+    public Optional<T> fetchVal(F param1, S param2) {
+        byte[] bytes = StorageUtil.readBytes(byteStorage, param1, param2);
+
+        T val;
+        if (bytes != null && (val = CodecUtil.decode(codec, bytes)) != null) {
+            return Optional.of(val);
+        }
+
+        return Optional.empty();
+    }
+
+    @Override
+    public boolean updateVal(F param1, S param2, T data) {
+        return StorageUtil.writeBytes(byteStorage, param1, param2, CodecUtil.encode(codec, data));
+    }
+
+}
