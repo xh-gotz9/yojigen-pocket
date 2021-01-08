fbb265c5a41da02cdc4bf1546e0d12df6da0518d
xh-gotz9 <xh353127981@gmail.com>
2021-01-08 22:35:34 +0800

    add RedisHashField into redis module

---
+++ pocket-redis/src/main/java/cn/gotz9/pocket/RedisHashField.java
@@ -0,0 +1,14 @@
+package cn.gotz9.pocket;
+
+import cn.gotz9.pocket.codec.ValueCodec;
+import cn.gotz9.pocket.storage.DataStorageProvider;
+import cn.gotz9.pocket.storage.redis.JedisHashStorage;
+import redis.clients.jedis.Jedis;
+
+public class RedisHashField<T, K, F> extends AbstractTwoParamAccessor<T, K, F> {
+
+    public RedisHashField(ValueCodec<T> codec, DataStorageProvider<Jedis> storageProvider, ParamConverter<K, String> keyConverter, ParamConverter<F, String> fieldConverter) {
+        super(codec, new JedisHashStorage(storageProvider), keyConverter, fieldConverter);
+    }
+
+}
